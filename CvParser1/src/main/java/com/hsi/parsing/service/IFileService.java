264e1a012eccb5f4aad49c158799b73deefa1608
sindhuja-mca <49112595+sindhuja-mca@users.noreply.github.com>
2019-06-18 16:05:20 +0530

    Update IFileService.java

--- CvParser1/src/main/java/com/hsi/parsing/service/IFileService.java
+++ CvParser1/src/main/java/com/hsi/parsing/service/IFileService.java
@@ -1,17 +1,12 @@
 package com.hsi.parsing.service;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.hsi.parsing.model.UploadDetails;
-
 @Service
-public interface IFileService {
+public interface IFileService{
 	
-
-			
-
-			UploadDetails fileUpload(MultipartFile file);
-
+	
+	void processFiles(MultipartFile[] files );
+	
 		
 }
